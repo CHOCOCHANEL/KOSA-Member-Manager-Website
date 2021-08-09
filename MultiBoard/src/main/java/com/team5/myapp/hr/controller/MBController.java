e8c8dfe27f4c9d0ae397ac7e738b02392329457d
Gram <Gram@DESKTOP-5H2VVJE>
2021-08-09 16:42:40 +0900

    (종현) MBController updateform

--- MultiBoard/src/main/java/com/team5/myapp/hr/controller/MBController.java
+++ MultiBoard/src/main/java/com/team5/myapp/hr/controller/MBController.java
@@ -1,5 +1,131 @@
 package com.team5.myapp.hr.controller;
 
-public enum MBController {
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.team5.myapp.hr.model.MBVO;
+import com.team5.myapp.hr.service.IMBService;
+
+@Controller
+public class MBController {
+	
+	@Autowired
+	IMBService mbService;
+
+
+	@RequestMapping(value = "/hr/count")
+	public String mbCount(@RequestParam(value = "jobid", required = false, 
+									defaultValue = "0") int jobid, Model model) {
+		
+		System.out.println("hr/count requested");
+		
+		if (jobid == 0) {
+			model.addAttribute("count", mbService.getMbCount());
+		} else {
+			model.addAttribute("count", mbService.getMbCount());
+		}
+		
+		return "hr/count"; // WEB-INF/views/hr/count.jsp 파일을 실행함
+	}
+	
+	/*구성원 목록 조회?*/
+	
+	@RequestMapping(value= {"/hr", "/hr/list"})
+	public String getAllmb(Model model) {
+		List<MBVO> mbList = mbService.getMbList();
+		model.addAttribute("empList", mbList);
+		return "hr/list";
+	}
+	
+	@RequestMapping(value="/hr/{employeeId}")
+	public String getMbInfo(@PathVariable int employeeId, Model model) {
+		MBVO mb = mbService.getMbInfo(employeeId);
+		model.addAttribute("mb", mb);
+		return "hr/view";
+	}
+	
+	/* 구성원정보 입력 */
+	// GET 방식(사원정보 입력 폼)과 POST 방식(입력받은 데이터 DB에 저장) 구분 처리
+	// 구성원 정보 뭐 입력할건지 정해서 넣어야함
+	
+	@RequestMapping(value="/hr/insert", method=RequestMethod.GET)
+	public String insertMb(Model model) {
+		model.addAttribute("deptList", mbService.getAllDeptId());
+		model.addAttribute("jobList", mbService.getAllJobId());
+		model.addAttribute("managerList", mbService.getAllManagerId());
+		return "hr/insertform";
+	}
+	
+	/* 구성원 정보 입력 처리 */
+	// 사원정보 입력 폼에서 저장하면 실행됨. view로 forward하지 않고, 사원정보를 저장한 뒤, 사원목록을 redirect함.
+	@RequestMapping(value="hr/insert", method=RequestMethod.POST)
+	public String insertMb(MBVO mb, Model model) {
+		mbService.insertMb(mb);
+		return "redirect:/hr";
+	}
+	
+	/* 구성원 정보 수정 */
+	// 어떤 정보 수정할건지 정해야함
+	@RequestMapping(value="hr/update", method=RequestMethod.GET)
+	public String updateEmp(int empid, Model model) {
+		model.addAttribute("emp", mbService.getEmpInfo(empid));
+		model.addAttribute("deptList", mbService.getAllDeptId());
+		model.addAttribute("jobList", mbService.getAllJobId());
+		model.addAttribute("managerList", mbService.getAllManagerId());
+		return "hr/updateform";
+	}
+	
+	/* 구성원 정보 수정처리 */
+	// 사원정보 수정 폼에서 저장하면 실행됨. view로 forward하지 않고, 사원정보를 수정한 뒤, 사원목로를 redirect함.
+	@RequestMapping(value="hr/update", method=RequestMethod.POST)
+	public String updateMb(MBVO mb, Model model) {
+		mbService.updateMb(mb);
+		return "redirect:/hr/" + mb.getMbId();
+	//	return "redirect:/hr"; // 수정 후 사원 목록조회 화면으로 이동
+	}
+	
+	/* 구성원 정보 삭제 */
+	// 삭제 시 이메일 입력 요구. GET방식으로 삭제 확인을 위한 이메일 입력 폼으로 forward함.
+	@RequestMapping(value="hr/delete", method=RequestMethod.GET)
+	public String deleteEmp(int empid, Model model) {
+		model.addAttribute("emp", mbService.getMbInfo(empid));
+		return "hr/deleteform";
+	}
+	
+	/* 구성원 정보 삭제처리 */
+	@RequestMapping(value="hr/delete", method=RequestMethod.POST)
+	public String deleteEmp(int mbid, String email, Model model) {
+		mbService.deleteEmp(mbid, email);
+		return "redirect:/hr";
+	}
+	
+	/* 구성원 과거 조 정보 조회*/
+	@RequestMapping(value= {"/hr", "/hr/past"})
+	public String getAllMb(Model model) {
+		List<MBVO> mbList = mbService.getMbList();
+		model.addAttribute("mbList", mbList);
+		return "hr/past";
+	}
+	
+	
+	/* 에러 처리 */
+	@ExceptionHandler({RuntimeException.class})
+	public ModelAndView runtimeException(HttpServletRequest request, Exception ex) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("url", request.getRequestURI());
+		mav.addObject("exception", ex);
+		mav.setViewName("error/runtime");
+		return mav;
+	}
 }
--- MultiBoard/src/main/webapp/WEB-INF/views_mb/updateform.jsp
+++ MultiBoard/src/main/webapp/WEB-INF/views_mb/updateform.jsp
@@ -1,12 +1,79 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>CoderBy Update Form</title>
 </head>
 <body>
+	<h1>구성원 정보 수정</h1>
+	<form action="./update" method="post">
+	<table border="1">
+	<tr>
+		<th>MB_ID</th>
+		<td><input type="number" name="mbId" value="${mb.mbId}" readonly></td>
+	</tr>
+	<tr>
+		<th>FIRST_NAME</th>
+		<td><input type="text" name="firstName" value="${mb.firstName}"></td>
+	</tr>
+	<tr>
+		<th>LAST_NAME</th>
+		<td><input type="text" name="lastName" value="${mb.lastName}" required></td>
+	</tr>
+	<tr>
+		<th>EMAIL</th>
+		<td><input type="text" name="email" value="${mb.email}" required></td>
+	</tr>
+	<tr>
+		<th>PHONE_NUMBER</th>
+		<td><input type="text" name="phoneNumber" value="${mb.phoneNumber}"></td>
+	</tr>
+	<tr>
+		<th>GROUP_ID</th>
+		<td><input type="text" name="groupId" value="${mb.groupId}"></td>
+	</tr>	
 
+	<tr>
+		<th>JOB_ID</th>
+		<td>
+		<select name="jobId">
+		<c:forEach var="job" items="${jobList}">
+			<c:if test="${mb.jobId == job.jobId}">
+				<option value="${job.jobId}" selected>${job.title}</option>
+			</c:if>
+			<c:if test="${mb.jobId != job.jobId}">
+				<option value="${job.jobId}">${job.title}</option>
+			</c:if>
+		</c:forEach>
+		</select>
+		</td>
+	</tr>
+	<tr>
+		<th>MANAGER_ID</th>
+		<td>
+			<select name="managerId">
+			<c:forEach var="manager" items="${managerList}">
+				<c:if test="${emp.managerId == manager.managerId}">
+					<option value="${manager.managerId}" selected>
+				</c:if>
+				<c:if test="${emp.managerId != manager.managerId}">
+					<option value="${manager.managerId}">${manager.firstName}</option>
+				</c:if>
+			</c:forEach>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<th>&nbsp;</th>
+		<td>
+			<input type="submit" value="수정">
+			<input type="reset" value="취소">
+		</td>
+	</tr>
+	</table>
+	</form>
 </body>
 </html>
