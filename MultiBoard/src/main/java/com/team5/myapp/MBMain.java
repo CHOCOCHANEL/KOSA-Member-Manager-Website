e785d819fa2c1ad9aa360b1e2831d95f568072ff
samsung <samsung@JAY-FLEX>
2021-08-10 09:13:55 +0900

    Test를위한MBMain

--- MultiBoard/src/main/java/com/team5/myapp/MBMain.java
+++ MultiBoard/src/main/java/com/team5/myapp/MBMain.java
@@ -3,6 +3,7 @@ package com.team5.myapp;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import com.team5.myapp.hr.model.MBVO;
 import com.team5.myapp.hr.service.IMBService;
 
 public class MBMain {
@@ -14,14 +15,16 @@ public class MBMain {
 		
 		System.out.println("== 멤버 수 조회");
 		System.out.println(mbService.getMBCount());
-		System.out.println("-- 1조 멤버 수 조회");
-		System.out.println(mbService.getMBCount(1));
+		System.out.println("-- 5조 멤버 수 조회");
+		System.out.println(mbService.getMBCount(5));
 		
 		System.out.println("== 1번 멤버의 정보 조회");
 		System.out.println(mbService.getMBInfo(1));
 		
 		System.out.println("== 사원 전체 정보 조회");
-		System.out.println(mbService.getMBList());
+		for(MBVO mb : mbService.getMBList()) {
+			System.out.println(mb);
+		}
 		
 		context.close();
 	}
